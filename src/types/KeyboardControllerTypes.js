144678597c17ab393d836545ac6bfa215bf252f3
Alexey Lebed <alexeylebed@Alexeys-MBP.ssf.bugfocus.com>
2022-10-21 19:32:09 -0700

    remove js files

--- src/types/KeyboardControllerTypes.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.keyboardArrowEvents = exports.moveEvents = void 0;
-exports.moveEvents = ["up", "down", "left", "right"];
-exports.keyboardArrowEvents = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"];
